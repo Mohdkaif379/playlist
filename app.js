d763b8a4ceae568538fa91764c022c8c30dd0001
Mohdkaif379 <mohdkaif8603@gmail.com>
2026-02-19 11:08:07 +0530

    updatde

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(
 app.use('/', indexRouter);
 app.use('/auth', authRouter);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
 
 module.exports = app;
